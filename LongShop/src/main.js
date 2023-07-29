c01da83a18c6497afdcc2aa9fdf454f513fd7a9c
fuyl <longqingmiyu@163.com>
2023-07-29 21:10:07 +0800

    增加了vant手机端组件库，修改main中的相关的配置信息

--- LongShop/src/main.js
+++ LongShop/src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {Button} from "vant"
+
+const app = createApp(App)
+
+app.use(Button)
+
+app.mount('#app')
